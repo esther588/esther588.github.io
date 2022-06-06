a5bdbfab51adddf0727e8c074d3379af3fd68647
Esther Cheng <esther588@berkeley.edu>
2022-06-06 09:40:10 -0700

    edit id names

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ const response = await fetch(url);
 const result = await response.json();
 for (let i in result) {
     languagesPercentage = getLanguagePercentage(result[i].name);
-    displayGraph(languagesPercentage);
+    displayGraph(result[i].name, languagesPercentage);
 }
 
 function getLanguagePercentage(repoName) {
@@ -24,6 +24,69 @@ function getLanguagePercentage(repoName) {
     return languagesPercentage;
 }
 
-function displayGraph(percentageArr) {
+function displayGraph(repoName, percentageArr) {
+    html = '';
 
+    document.getElementById("graphContainer").innerHTML = '&nbsp;';
+
+    html += '<canvas id="' + repoName + 'Line" style="width:100%;max-width:600px"></canvas>';
+    html += '<canvas id="' + repoName + 'Bar" style="width:100%;max-width:600px"></canvas>';
+    html += '<canvas id="' + repoName + 'Pie" style="width:100%;max-width:600px"></canvas>';
+
+    document.getElementById("acneContainer").innerHTML = html;
+
+    var elem = document.getElementById(repoName + "Line").getContext("2d");
+    var elem1 = document.getElementById(repoName + "Bar").getContext("2d");
+    var elem2 = document.getElementById(repoName + "Pie").getContext("2d");
+
+    const newChart = new Chart(elem, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: acneColors,
+            data: acneValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: acneTitle
+            }
+        }
+    });
+
+    const newChart1 = new Chart(elem1, {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: acneColors,
+            data: acneValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: acneTitle
+            }
+        }
+    });
+
+    const newChart2 = new Chart(elem2, {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: acneColors,
+            data: acneValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: acneTitle
+            }
+        }
+    });
 }
