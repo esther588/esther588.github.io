7b60b07d0771139472973f269f796cff6cc77047
Esther Cheng <esther588@berkeley.edu>
2022-06-07 13:14:38 -0700

    turn function to async

--- data/data.js
+++ data/data.js
@@ -16,6 +16,24 @@ function createElement(repoName) {
 
 func();
 
+async function getLanguagePercentage(repoName) {
+    const ls = await fetch("https://api.github.com/repos/" + repoName + "/languages");
+    const languageStats = await ls.json();
+
+    const totalPtsArr = Object.values(languageStats);
+    var totalPts = 0;
+    totalPtsArr.forEach((pts) => {
+        totalPts += pts;
+    });
+
+    const languagesPercentage = {};
+    Object.keys(languageStats).forEach((language) => {
+        languagesPercentage[language] = (languageStats[language] * 100) / totalPts;
+    });
+
+    return languagesPercentage;
+}
+
 function displayGraph(repoName, percentageObj) {
     var xValues = Object.keys(percentageObj);
     var yValues = Object.values(percentageObj);
